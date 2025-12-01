79761cd31183070fbd3e1bd0367a164439fae1b0
Pedro Albuquerque <a7buquerque@gmail.com>
2025-12-01 17:36:07 -0300

    Alterações feitas no laboratório

--- client/udp_cilent.py
+++ client/udp_cilent.py
@@ -1,59 +1,79 @@
 import struct
 import socket
 
-HOST = '127.0.0.1'
-PORT = 60001
+HOST = '10.25.1.115'
+PORT = 60000
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    file_name = input("Informe o nome do arquivo para download: ")
-    if not file_name:
-        print("Informe um nome!")
-        exit(1)
-
-    file_name_length = len(file_name)
-
-    # Verifica se o nome não ultrapassa 1 byte
-    if file_name_length > 255:
-        print("O nome do arquivo é muito longo (máximo de 255 caracteres)!")
-        exit(1)
-
-    fl_bytes = struct.pack('>B', file_name_length)
-    s.sendto(fl_bytes, (HOST, PORT))
-
-    fn_bytes = file_name.encode('utf-8')
-    s.sendto(fn_bytes, (HOST, PORT))
-
-    print(f"Arquivo soolicitado: {file_name}")
-
     try:
-        status_data, addr = s.recvfrom(1)
-    except socket.timeout:
-        print("ERRO: tempo de espera excedido.")
-        exit(1)
-    except socket.ConnectionRefusedError:
-        print("ERRO: Conexão perdida: porta inacessível")
+        file_name = input("Informe o nome do arquivo para download: ")
+        if not file_name:
+            print("Informe um nome!")
+            exit(1)
 
-    status = struct.unpack('>B', status_data)[0]
+        file_name_length = len(file_name)
 
-    if status == 1:
-        print("Arquivo não existe.")
-    elif status == 0:
-        print("Arquivo encontrado! Iniciando o download.")
-    else:
-        print("Erro durante a requisição do nome de arquivo.")
+        # Verifica se o nome não ultrapassa 1 byte
+        if file_name_length > 255:
+            print("O nome do arquivo é muito longo (máximo de 255 caracteres)!")
+            exit(1)
 
-    try:
-        file_size_bytes, addr = s.recvfrom(4)
-    except socket.timeout:
-        print("ERRO: tempo de espera excedido.")
-        exit(1)
-    except socket.ConnectionRefusedError:
-        print("ERRO: Conexão perdida: porta inacessiva")
-        exit(1)
+        fl_bytes = struct.pack('>B', file_name_length)
+        s.sendto(fl_bytes, (HOST, PORT))
 
-    if len(file_size_bytes) != 4:
-        print("tamanho de arquivo inválido.")
-        exit(1)
+        fn_bytes = file_name.encode('utf-8')
+        s.sendto(fn_bytes, (HOST, PORT))
 
-    file_size = struct.unpack('>I', file_size_bytes)[0]
-    print(f"Tamanho do arquivo: {file_size} bytes")
+        print(f"Arquivo soolicitado: {file_name}")
+
+        try:
+            status_data, addr = s.recvfrom(1)
+        except socket.timeout:
+            print("ERRO: tempo de espera excedido.")
+            exit(1)
+
+        status = struct.unpack('>B', status_data)[0]
+
+        if status == 0:
+            print("Arquivo não existe.")
+        elif status == 1:
+            print("Arquivo encontrado! Iniciando o download.")
+        else:
+            print("Erro durante a requisição do nome de arquivo.")
+
+        try:
+            file_size_bytes, addr = s.recvfrom(4)
+        except socket.timeout:
+            print("ERRO: tempo de espera excedido.")
+            exit(1)
+        except socket.ConnectionRefusedError:
+            print("ERRO: Conexão perdida: porta inacessiva")
+            exit(1)
+
+        if len(file_size_bytes) != 4:
+            print("tamanho de arquivo inválido.")
+            exit(1)
+
+        file_size = struct.unpack('>I', file_size_bytes)[0]
+            
+        print(f"Tamanho do arquivo: {file_size} bytes")
+
+        file_data = b''
+        bytes_received = 0
+
+        while bytes_received < file_size:
+            try: 
+                chunk, _ = s.recvfrom(4096)
+            except socket.timeout:
+                print(f"ERRO: tempo de espera excedido!")
+                exit(1)
+            file_data += chunk
+            bytes_received += len(chunk)
+
+        output_name = f"baixado_{file_name}"
+
+        with open(output_name, 'wb') as f:
+                f.write(file_data)
+    except KeyboardInterrupt:
+        print("\nEncerrando...")
+
